e729dfca961371cd56bbff5e22d2b7caf2f96750
Dennis Okparaocha <dennis.okparaocha@olmgroup.com>
2018-05-08 20:05:04 +0100

    setup calendar

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/navbar.css';
+import './styles/navbar-top.css';
+import './styles/calendar.css';
 import './styles/index.css';
 import Provider from './components/Provider';
 import registerServiceWorker from './registerServiceWorker';
