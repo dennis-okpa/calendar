2d16d2290ac161287585e1f609f1cd768833c266
Dennis Okparaocha <dennis_okpa@hotmail.co.uk>
2018-09-20 20:04:00 +0000

    add enviroment property file

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,6 @@
+require('dotenv/config');
+console.log("knex.js process.env.NODE_ENV", process.env.NODE_ENV);
+
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile');
 const environmentConfig = config[environment.trim()];
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,20 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: 'postgres://postgres:postgres@localhost:5432/calendar'
-  },
-  test: {
-    client: 'pg',
-    connection: 'postgres://postgres:postgres@localhost:5432/test-calendar'
-  },
+    development: {
+        client: 'pg',
+        connection: 'postgres://postgres:postgres@localhost:5432/calendar'
+    },
+    production: {
+        client: 'pg',
+        connection: 'postgres://postgres:postgres@localhost:5432/calendar'
+    },
+    test: {
+        client: 'pg',
+        connection: 'postgres://postgres:postgres@localhost:5432/test-calendar'
+    },
+    test_sh: {
+        client: 'pg',
+        connection: 'postgres://postgres:postgres@localhost:5432/test-calendar'
+    }
 };
